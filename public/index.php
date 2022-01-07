0ecc2eb2e6a992fd017dc3f099ef9f133558623a
Andrew Wippler <Andrew.Wippler@users.github.com>
2022-01-07 13:49:39 -0800

    Google auth

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../config.php';
 use ICanBoogie\DateTime; // https://packagist.org/packages/icanboogie/datetime
 use HUD\ExtendedFamily;
+use AliasProject\Google\Calendar as gcal;
 
 $now = new DateTime('now', $my_timezone);
 
@@ -12,9 +13,44 @@ foreach ($all_family as $fam)
     $family_display[] = new ExtendedFamily($fam);
 }
 
-// var_dump($family_display);
+$gcal = new gcal($google_config);
+$events = $gcal->listEvents($google_calendar_email, 'today', $my_timezone);
 
-// $family_display[0]->can_call();
+
+$events_array = [];
+for ($i=0; $i < count($events); $i++) { 
+    // Revisions show up twice on the calendar and have _R<date> appended to the ID.
+    // This is to clear up duplicates by forcing in each event into one array of
+    // the same ID. This uses the latest revision of the event.
+    $array_id = explode("_", $events[$i]->id)[0];
+
+   if(!empty($events[$i]->start->dateTime)) 
+   {
+
+    $start = new DateTime($events[$i]->start->dateTime);
+    $end = new DateTime($events[$i]->end->dateTime);
+
+    $events_array[$array_id] = [
+        "summary" => $events[$i]->summary,
+        "start" => $start->as_time,
+        "end" => $end->as_time
+    ];
+   } 
+   elseif (!empty($events[$i]->start->date)) 
+   {
+    // all-day events
+    $events_array[$array_id] = [
+        "summary" => $events[$i]->summary,
+        "start" => $events[$i]->start->date,
+        "end" => null
+    ];
+   }
+
+}
+
+// Calendar items:
+// var_dump($events_array);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
